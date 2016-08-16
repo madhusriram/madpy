898967054056303e1507e629d2d51c82ca3759d3
madhusirmv <madhusirmv@gmail.com>
2016-08-16 00:00:49 -0400

    templates for hw2

---
+++ cs253/hw2_templates/main.py
@@ -0,0 +1,30 @@
+import webapp2
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **kw):
+		t = jinja_env.get_template(template)
+		return t.render(kw)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class ShoppingHandler(Handler):
+	def get(self):
+		items = self.request.get_all('food')
+		self.render('shopping_list.html', items = items)
+		
+class FizzBuzz(Handler):
+	def get(self):
+		n = self.request.get('n', 0)
+		n = n and int(n)
+		self.render('fizzbuzz.html', n=n)
+
+app = webapp2.WSGIApplication([('/', ShoppingHandler), ('/fizzbuzz', FizzBuzz)], debug=True)
